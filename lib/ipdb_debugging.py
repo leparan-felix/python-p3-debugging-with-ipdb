f22cdcff44e73e4930a44f4068be9ba9304777dc
leparan <leparanfelix16@gmail.com>
2025-05-07 13:30:46 +0300

    changes

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import ipdb
+# import ipdb
 
 def plus_two(num):
-    num + 2
+    num = num + 2  # Corrected operation
     return num
--- lib/testing/ipdb_debugging_test.py
+++ lib/testing/ipdb_debugging_test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-from ipdb_debugging import plus_two
+from ipdb_debugging import plus_two  # Import the function
 
 class TestIpdbDebugging:
-    '''ipdb_debugging.py'''
-    
+    '''Tests for ipdb_debugging.py'''
+
     def test_adds_two(self):
-        '''adds_two() adds 2 to input arg and returns sum.'''
-        assert(plus_two(3) == 5)
+        '''Test that plus_two() adds 2 to the input argument and returns the correct result.'''
+        result = plus_two(3)  # Call the function
+        assert result == 5     # Assert that the result is 5
